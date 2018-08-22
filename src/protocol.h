64e859eb9c9c382008dcfe2ab9d9729431308fd5
Michael Polzer <>
2018-08-22 10:36:51 +0200

    prefer to connect to upgraded nodes

--- src/protocol.h
+++ src/protocol.h
@@ -324,7 +324,7 @@ enum ServiceFlags : uint64_t {
  * should be updated appropriately to filter for the same nodes.
  */
 static ServiceFlags GetDesirableServiceFlags(ServiceFlags services) {
-    return ServiceFlags(NODE_NETWORK/* | NODE_WITNESS*/); // PM-Tech: for now let's connect non-witness nodes as well
+    return ServiceFlags(NODE_NETWORK | NODE_WITNESS);
 }
 
 /**
